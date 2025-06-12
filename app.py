89dd989f38e09fcbcdc6840087f21f951e004934
dilnawazmustafa <dilnawaz.mustafa@gmail.com>
2025-06-12 17:58:29 -0600

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,9 @@ import pickle
 import pandas as pd
 
 # Load trained model
-model = pickle.load(open('retail_model.pkl', 'rb'))
+model_path = os.path.join(os.path.dirname(__file__), 'retail_model.pkl')
+with open(model_path, 'rb') as f:
+    model = pickle.load(f)
 
 st.title("🛍️ Retail Sales Prediction App")
 st.write("Enter details to predict the total purchase amount.")
